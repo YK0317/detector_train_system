01ac2e252f9ed6a195ab9d379db5dac9bfbba727
YK030107 <ykeong0107@1utar.my>
2025-08-04 17:43:22 +0800

    install tested

--- train_system/build/lib/build/lib/build/lib/train_system/api/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-API module for Train System
-
-Provides REST API functionality for remote training control.
-"""
-
-from .server import TrainingAPI, create_app, run_server
-
-__all__ = ["TrainingAPI", "create_app", "run_server"]
